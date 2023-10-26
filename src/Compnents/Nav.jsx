696ea1341135361eef00fe947a60c49d4afa4541
Carl-with-a-C <carlbclifton@gmail.com>
2023-10-26 23:16:28 +0100

    clean up darkmode

--- src/Compnents/Nav.jsx
+++ src/Compnents/Nav.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { motion } from "framer-motion";
 
 const Nav = ({ theme, setTheme }) => {
@@ -5,15 +6,63 @@ const Nav = ({ theme, setTheme }) => {
   const showTime = date.getHours() + ":" + date.getMinutes();
   const showAmPm = date.getHours() >= 12 ? "pm" : "am";
 
+  const [isOn, setIsOn] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
   const switchTheme = () => {
     const newTheme = theme === "light" ? "dark" : "light";
+    setIsOn(!isOn);
     setTheme(newTheme);
   };
 
-  console.log(theme);
+  const spring = {
+    type: "spring",
+    stiffness: 700,
+    damping: 30,
+  };
+
+  const linksContainer = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.2,
+        ease: [0.78, 0.01, 0.21, 1],
+      },
+    },
+  };
+
+  const links = {
+    hidden: { rotateX: "-90deg", y: "16px" },
+    show: {
+      rotateX: "0deg",
+      y: "0px",
+      transition: {
+        type: "spring",
+        bounce: 0,
+        duration: 0.75,
+        delay: 0.5,
+        ease: [0.78, 0.01, 0.21, 1],
+      },
+    },
+  };
+
+  const linksCover = {
+    hidden: { y: "0%" },
+    show: {
+      y: "100%",
+      transition: {
+        type: "spring",
+        bounce: 0,
+        duration: 0.75,
+        delay: 0.5,
+        ease: [0.78, 0.01, 0.21, 1],
+      },
+    },
+  };
 
   return (
-    <header className="header">
+    <motion.header className="header" data-menuOpen={menuOpen} layout>
       <motion.nav
         className="nav-container container"
         initial={{ opacity: 0 }}
@@ -48,18 +97,111 @@ const Nav = ({ theme, setTheme }) => {
           </div>
         </div>
         <div className="lightmode-switch-container">
-          <label className="switch">
-            <input type="checkbox" onClick={switchTheme} />
-            <span className="slider round"></span>
-          </label>
+          <div className="switch" data-isOn={isOn} onClick={switchTheme}>
+            <motion.div className="handle" layout transition={spring} />
+          </div>
         </div>
         <div className="menu-btn-container">
-          <button>
-            <h4>menu</h4>
+          <button
+            onClick={() => {
+              setMenuOpen(!menuOpen);
+            }}
+          >
+            <div className="menu-btn-text-container" data-menuOpen={menuOpen}>
+              <motion.div
+                className="menu-btn-text"
+                style={menuOpen ? { height: "0%" } : { height: "100%" }}
+                layout
+              >
+                <h4>menu</h4>
+                <h4>close</h4>
+              </motion.div>
+            </div>
           </button>
         </div>
+        <motion.div
+          className="drop-down-menu-container container"
+          style={menuOpen ? { y: "0%" } : { y: "-100%" }}
+        >
+          <div className="dropdown-menu-links-container">
+            <div className="sitemap-links-container">
+              <motion.ul
+                className="sitemap-links"
+                variants={linksContainer}
+                initial="hidden"
+                animate={menuOpen ? "show" : "hidden"}
+                layout
+              >
+                <motion.li variants={links}>
+                  <motion.h1>Home</motion.h1>
+                  <motion.div
+                    className="links-cover"
+                    variants={linksCover}
+                    initial="hidden"
+                    animate={menuOpen ? "show" : "hidden"}
+                  ></motion.div>
+                </motion.li>
+                <motion.li variants={links}>
+                  <motion.h1>About</motion.h1>
+                  <motion.div
+                    className="links-cover"
+                    variants={linksCover}
+                    initial="hidden"
+                    animate={menuOpen ? "show" : "hidden"}
+                  ></motion.div>
+                </motion.li>
+                <motion.li variants={links}>
+                  <motion.h1>Projects</motion.h1>
+                  <motion.div
+                    className="links-cover"
+                    variants={linksCover}
+                    initial="hidden"
+                    animate={menuOpen ? "show" : "hidden"}
+                  ></motion.div>
+                </motion.li>
+                <motion.li variants={links}>
+                  <motion.h1>Services</motion.h1>
+                  <motion.div
+                    className="links-cover"
+                    variants={linksCover}
+                    initial="hidden"
+                    animate={menuOpen ? "show" : "hidden"}
+                  ></motion.div>
+                </motion.li>
+
+                <motion.li variants={links}>
+                  <motion.h1>Contact</motion.h1>
+                  <motion.div
+                    className="links-cover"
+                    variants={linksCover}
+                    initial="hidden"
+                    animate={menuOpen ? "show" : "hidden"}
+                  ></motion.div>
+                </motion.li>
+              </motion.ul>
+            </div>
+            <motion.div
+              className="socials-links-container"
+              initial={{ opacity: 0 }}
+              animate={menuOpen ? { opacity: 1 } : { opacity: 0 }}
+              transition={{ duration: 0.25 }}
+            >
+              <ul className="socials-links">
+                <li>
+                  <h3>Behance</h3>
+                </li>
+                <li>
+                  <h3>Instagram</h3>
+                </li>
+                <li>
+                  <h3>Linkedin</h3>
+                </li>
+              </ul>
+            </motion.div>
+          </div>
+        </motion.div>
       </motion.nav>
-    </header>
+    </motion.header>
   );
 };
 
